df3b9070e4dd781ecc95d34f3164e13afcc570c7
lis19 <39839829+lis19@users.noreply.github.com>
2024-05-13 23:05:23 +0000

    ch1 complete

--- Ch01/01_03/bad_values.py
+++ Ch01/01_03/bad_values.py
@@ -2,7 +2,7 @@
 import pandas as pd
 
 # %%
-df = pd.read_csv('metrics.csv', parse_dates=['time'])
+df = pd.read_csv('/workspaces/data-cleaning-in-python-essential-training-3086536/Ch01/01_03/metrics.csv', parse_dates=['time'])
 df.sample(10)
 
 # %%
@@ -18,8 +18,7 @@ pd.pivot(df, index='time', columns='name').plot(subplots=True)
 df.query('name == "cpu" & (value < 0 | value > 100)')
 
 # %% 
-mem = df[df['name'] == 'mem']['value']
-z_score = (mem - mem.mean())/mem.std()
-bad_mem = mem[z_score.abs() > 2]
-# bad_mem
-df.loc[bad_mem.index]
+mem = df[df['name'] == 'mem']['value'] # filter by mem only in 'name' column, and save 'value' entries 
+z_score = (mem - mem.mean())/mem.std() # z-score (distance from mean)
+bad_mem = mem[z_score.abs() > 2] # find those that are >2s.d. away
+df.loc[bad_mem.index] # use index column of bad_mem to find bad rows in df
